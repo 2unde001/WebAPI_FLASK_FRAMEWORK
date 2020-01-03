9d8d186da3f078a411c21ac8f3950318cef0e8f9
Tunde Hammed <tunde0011@hotmail.com>
2020-01-03 12:09:31 +0000

    retrieved data from the db

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask import request,jsonify
+import sqlite3
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 """
 Create a dictionaries test data for our catalog
@@ -38,8 +45,35 @@ def home():
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+     return jsonify(books)
+
+@app.route(404):
+def page_not_found(e):
+    return '<h1>404<h1/><p>The resources could not be found,</p>',404
 
 
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    """
+    Check if an ID was provided as part of the Url
+    Assign ID provided to a variable and if ID not provided throw an error
+    If ID is not provided, display an error in the browser
+
+    """
+
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return 'Error: No id field provided. Please specify an id.'
+
+    # Create an empty list for result
+    results = []
+
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # add result to jsonify funcion
+    return jsonify(results)
 
 app.run()
